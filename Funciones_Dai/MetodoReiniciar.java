63b3eadb75666610e33cc180b91b69f5ff185bda
Daiana Serafini <daiana.serafini@alumnos.frm.utn.edu.ar>
2019-06-11 21:54:52 -0300

    Aca esta el otro metodo para completar

---
+++ Funciones_Dai/MetodoReiniciar.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author david
+ */
+public class MetodoReiniciar {
+    private void reiniciar(){
+        carta1.setEnabled(true);
+        carta2.setEnabled(true);
+        carta3.setEnabled(true);
+        carta4.setEnabled(true);
+        carta5.setEnabled(true);
+        carta6.setEnabled(true);
+        carta7.setEnabled(true);
+        carta8.setEnabled(true);
+        carta9.setEnabled(true);
+        carta10.setEnabled(true);
+        carta11.setEnabled(true);
+        carta12.setEnabled(true);
+        carta13.setEnabled(true);
+        carta14.setEnabled(true);
+        carta15.setEnabled(true);
+        carta16.setEnabled(true);
+        carta17.setEnabled(true);
+        carta18.setEnabled(true);
+        carta19.setEnabled(true);
+        carta20.setEnabled(true);
+        
+        primeraC=false;
+        voltear=false;
+        
+        
+        //agregar a gui > Juego
+    }
